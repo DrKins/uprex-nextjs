26b108edeebe3747af088a0d257d628b30ba419c
DrKins <fade.sestan@gmail.com>
2021-11-18 12:15:08 +0100

    fix(image/next priority ): faster loading of next/image

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,18 @@ import type { NextPage } from 'next'
 import { useRouter } from 'next/dist/client/router'
 import Landingstyles from '../styles/Landing.module.css'
 import Image from "next/image";
+import LogoImage from "../public/Card.png";
 const Landing: NextPage = () => {
   const router = useRouter();
   return (
     <div>
       <Image
-        src="/Card.png"
+        src={LogoImage}
         alt="image"
         width={186}
         height={158}
         layout="fixed"
+        priority
       />
       <h1>Uprex</h1>
       <p>Enjoy neat web gaming expirience.</p>
