a9c5d540eeccc68d2ca84ea72956f0fd9391e217
DrKins <fade.sestan@gmail.com>
2021-11-19 19:48:19 +0100

    feat(full game flow added): after three rounds game will stop else it will continue to next round
    On display there will be displayed if you lost or won round. On last round it will display lost or won game or draw.

--- pages/finish.tsx
+++ pages/finish.tsx
@@ -1,26 +1,53 @@
 import { NextPage } from "next";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
+import { gameContext } from "../context";
 
 const Finish: NextPage = () => {
+  const [winner, setWinner] = useState("It's draw game.");
+  const [last, setLast] = useState(false);
+  const { rounds, updateRounds } = useContext(gameContext);
   const router = useRouter();
-  let [time, setTime] = useState(10);
+  let [time, setTime] = useState(5);
+
   function Timer() {
     let TIMER_1 = setInterval(() => {
       setTime(time--);
       if (time === -1) {
-        clearInterval(TIMER_1);
-        router.push("/selection");
+        //three rounds per game, game over
+        if (rounds.length === 3) {
+          //sum of array to see winner function
+          let x = rounds.reduce((a, b) => a + b, 0);
+          if (x < 0) setWinner("You lost game.");
+          else if (x > 0) setWinner("You win game.");
+          updateRounds(0, true);
+          clearInterval(TIMER_1);
+          router.push("/");
+        } else {
+          clearInterval(TIMER_1);
+          router.push("/selection");
+        }
       }
     }, 1000);
   }
   useEffect(() => {
+    if (rounds.length === 3) setLast(true);
+    console.log(rounds);
     Timer();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
       <div>Round finished.</div>
+      <h2>
+        {last
+          ? winner
+          : rounds[rounds.length - 1] > 0
+          ? "You win this round"
+          : rounds[rounds.length - 1] < 0
+          ? "You lost this round"
+          : "It's draw round"}
+      </h2>
       <div>Next round starts in {time}</div>
     </>
   );
