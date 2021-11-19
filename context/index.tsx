7e697c2992a15f44329a5f2d57265ab84f620208
DrKins <fade.sestan@gmail.com>
2021-11-19 13:17:32 +0100

    feat(context index implementation of opponent states): Opponent ball and opponent card states with mutations

--- context/index.tsx
+++ context/index.tsx
@@ -3,23 +3,23 @@ import { createContext, FC, useContext, useState } from "react";
 type gameContextType = {
   userBall: Number;
   userCard: Number;
-  //   opponentBall: Number;
-  //   opponentCard: Number;
+  opponentBall: Number;
+  opponentCard: Number;
   setUserBall: (newUserBall: number) => void;
   setUserCard: (newUserCard: number) => void;
-  //   setOpponentBall: () => void;
-  //   setOpponentCard: () => void;
+  setOpponentBall: (newOpponentBall: number) => void;
+  setOpponentCard: (newOpponentCard: number) => void;
 };
 
 const gameContextDefaultValues: gameContextType = {
   userBall: 0,
   userCard: 0,
-  //   opponentBall: 0,
-  //   opponentCard: 0,
+  opponentBall: 0,
+  opponentCard: 0,
   setUserBall: (newUserBall: number) => {},
-  //   setOpponentBall: () => {},
+  setOpponentBall: (newOpponentBall: number) => {},
   setUserCard: (newUserCard: number) => {},
-  //   setOpponentCard: () => {},
+  setOpponentCard: (newOpponentCard: number) => {},
 };
 
 export const gameContext = createContext<gameContextType>(
@@ -33,15 +33,29 @@ export function useGame() {
 const GameProvider: FC = ({ children }) => {
   const [userBall, setuserBall] = useState(gameContextDefaultValues.userBall);
   const [userCard, setuserCard] = useState(gameContextDefaultValues.userCard);
+  const [opponentBall, setopponentBall] = useState(
+    gameContextDefaultValues.opponentBall
+  );
+  const [opponentCard, setopponentCard] = useState(
+    gameContextDefaultValues.opponentCard
+  );
 
   const setUserBall = (newUserBall: number) => setuserBall(newUserBall);
   const setUserCard = (newUserCard: number) => setuserCard(newUserCard);
+  const setOpponentBall = (newOpponentBall: number) =>
+    setopponentBall(newOpponentBall);
+  const setOpponentCard = (newOpponentCard: number) =>
+    setopponentCard(newOpponentCard);
 
   return (
     <gameContext.Provider
       value={{
         userBall,
         userCard,
+        opponentBall,
+        opponentCard,
+        setOpponentBall,
+        setOpponentCard,
         setUserBall,
         setUserCard,
       }}
