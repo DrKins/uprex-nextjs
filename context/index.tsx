68492380ba162f084f7496abb4afd70208f5d99a
DrKins <fade.sestan@gmail.com>
2021-11-19 17:53:36 +0100

    feat(new states ordering flow of game): state rounds and updateRound function

--- context/index.tsx
+++ context/index.tsx
@@ -5,10 +5,12 @@ type gameContextType = {
   userCard: Number;
   opponentBall: Number;
   opponentCard: Number;
+  rounds: Number[];
   setUserBall: (newUserBall: number) => void;
   setUserCard: (newUserCard: number) => void;
   setOpponentBall: (newOpponentBall: number) => void;
   setOpponentCard: (newOpponentCard: number) => void;
+  updateRounds: (addRound: number) => void;
 };
 
 const gameContextDefaultValues: gameContextType = {
@@ -16,10 +18,12 @@ const gameContextDefaultValues: gameContextType = {
   userCard: 0,
   opponentBall: 0,
   opponentCard: 0,
+  rounds: [],
   setUserBall: (newUserBall: number) => {},
   setOpponentBall: (newOpponentBall: number) => {},
   setUserCard: (newUserCard: number) => {},
   setOpponentCard: (newOpponentCard: number) => {},
+  updateRounds: (addRound: number) => {},
 };
 
 export const gameContext = createContext<gameContextType>(
@@ -39,6 +43,7 @@ const GameProvider: FC = ({ children }) => {
   const [opponentCard, setopponentCard] = useState(
     gameContextDefaultValues.opponentCard
   );
+  const [rounds, setRounds] = useState(gameContextDefaultValues.rounds);
 
   const setUserBall = (newUserBall: number) => setuserBall(newUserBall);
   const setUserCard = (newUserCard: number) => setuserCard(newUserCard);
@@ -47,13 +52,16 @@ const GameProvider: FC = ({ children }) => {
   const setOpponentCard = (newOpponentCard: number) =>
     setopponentCard(newOpponentCard);
 
+  const updateRounds = (addRound: number) => setRounds([...rounds, addRound]);
   return (
     <gameContext.Provider
       value={{
         userBall,
         userCard,
+        rounds,
         opponentBall,
         opponentCard,
+        updateRounds,
         setOpponentBall,
         setOpponentCard,
         setUserBall,
