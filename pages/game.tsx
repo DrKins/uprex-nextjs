0a3ff8759371d29bc1178fbc8b522029b895e2d7
DrKins <fade.sestan@gmail.com>
2021-11-19 18:01:49 +0100

    feat(dynamic presentation of cards): cards will depend on state values opponentCard and userCard

--- pages/game.tsx
+++ pages/game.tsx
@@ -5,19 +5,31 @@ import { gameContext } from "../context/index";
 import { useContext } from "react";
 
 const Game: NextPage = () => {
-  const { userBall } = useContext(gameContext);
+  const { userBall, userCard, opponentBall, opponentCard } =
+    useContext(gameContext);
   return (
     <div className={styles.gameContainer}>
       <div className={styles.PlayerUI}>
         <div className={styles.PlayerCard}>
-          <Image
-            src="/CardUp.png"
-            alt="Card up"
-            height={75}
-            width={50}
-            layout="fixed"
-            priority={true}
-          />
+          {userCard > 0 ? (
+            <Image
+              src="/CardUp.png"
+              alt="Card up"
+              height={75}
+              width={50}
+              layout="fixed"
+              priority={true}
+            />
+          ) : (
+            <Image
+              src="/CardDown.png"
+              alt="Card down"
+              height={75}
+              width={50}
+              layout="fixed"
+              priority={true}
+            />
+          )}
         </div>
         <div className={styles.PlayerLine}>Your hand</div>
         <div className={styles.PlayerNumber}>
@@ -27,20 +39,31 @@ const Game: NextPage = () => {
       <div className={styles.OpponentsUI}>
         <p>Opponents Number</p>
         <div className={styles.OpponentsNumber}>
-          <div className={styles.OpponentsBall}>22</div>
+          <div className={styles.OpponentsBall}>{opponentBall}</div>
         </div>
       </div>
       <div className={styles.OpponentsUI}>
         <p>Opponents Play</p>
         <div className={styles.OpponentsCard}>
-          <Image
-            src="/CardDown.png"
-            alt="Card up"
-            height={221}
-            width={150}
-            layout="fixed"
-            priority={true}
-          />
+          {opponentCard > 0 ? (
+            <Image
+              src="/CardUp.png"
+              alt="Card up"
+              height={221}
+              width={150}
+              layout="fixed"
+              priority={true}
+            />
+          ) : (
+            <Image
+              src="/CardDown.png"
+              alt="Card down"
+              height={221}
+              width={150}
+              layout="fixed"
+              priority={true}
+            />
+          )}
         </div>
       </div>
     </div>
