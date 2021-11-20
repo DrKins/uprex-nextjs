eaf58ce94ea66e28515eea882b8f715803c4b033
DrKins <fade.sestan@gmail.com>
2021-11-20 22:46:34 +0100

    fix(new flow of DOM, no more url pages): all components and controller state being used

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,28 +3,52 @@ import { useRouter } from 'next/dist/client/router'
 import Landingstyles from '../styles/Landing.module.css'
 import Image from "next/image";
 import LogoImage from "../public/Card.png";
+import Selection from "../components/selection";
+import Game from "../components/game";
+import Finish from "../components/finish";
+import { gameContext } from "../context";
+import { useContext } from "react";
 const Landing: NextPage = () => {
-  const router = useRouter();
-  return (
-    <div>
-      <Image
-        src={LogoImage}
-        alt="image"
-        width={186}
-        height={158}
-        layout="fixed"
-        priority={true}
-      />
-      <h1>Uprex</h1>
-      <p>Enjoy neat web gaming expirience.</p>
-      <button
-        className={Landingstyles.btnStart}
-        onClick={() => router.push("/selection")}
-      >
-        START
-      </button>
-    </div>
-  );
+  const { controller, updateController } = useContext(gameContext);
+  function main() {
+    switch (controller) {
+      case 0:
+        return (
+          <div>
+            <Image
+              src={LogoImage}
+              alt="image"
+              width={186}
+              height={158}
+              layout="fixed"
+              priority={true}
+            />
+            <h1>Uprex</h1>
+            <p>Enjoy neat web gaming expirience.</p>
+            <button
+              className={Landingstyles.btnStart}
+              onClick={() => updateController(1)}
+            >
+              START
+            </button>
+          </div>
+        );
+        break;
+      case 1:
+        return <Selection />;
+        break;
+      case 2:
+        return <Game />;
+        break;
+      case 3:
+        return <Finish />;
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+  return <>{main()}</>;
 };
 
 export default Landing
